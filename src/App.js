bbcf8c8bb25b24fb369b7e55a8fb30a34b6cf55d
John Pena <pemu775@gmail.com>
2019-06-06 11:45:14 -0400

    Got the input slider working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Metronome from './Metronome';
+import Slider from './Slider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <React.Fragment>
+
+    <Slider />
+
+    <Metronome
+      tempo={120}
+      render={({
+        tempo,
+        beatsPerMeasure,
+        playing,
+        beat,
+        onPlay,
+        onTempoChange,
+      }) => (
+          <React.Fragment>
+            <div>
+              {tempo} <small>BPM</small>
+            </div>
+            <div>
+              {beatsPerMeasure}/{beatsPerMeasure} <small>T.S.</small>
+            </div>
+
+            <div>
+              <input
+                type="range"
+                min={40}
+                max={240}
+                value={tempo}
+                onChange={event => onTempoChange(event.target.value)}
+              />
+
+              <span>
+                {beat}/{beatsPerMeasure}
+              </span>
+
+              <button onClick={onPlay}>
+                {playing ? 'Pause' : 'Play'}
+              </button>
+            </div>
+          </React.Fragment>
+        )}
+    />
+  </React.Fragment>
+);
 
 export default App;
---
+++ src/Slider/indexBasic.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import sliderStyles from './slider.module.css';
+
+class Slider extends Component {
+
+  showSliderValue = () => {
+    // var rangeSlider = document.getElementById("rs-range-line");
+    const slider = this.input;
+    // var rangeBullet = document.getElementById("rs-bullet");
+    const bullet = this.bullet;
+
+    bullet.innerHTML = slider.value;
+    // rangeBullet.innerHTML = rangeSlider.value;
+
+    // var bulletPosition = (rangeSlider.value / rangeSlider.max);
+    const bulletPosition = slider.value / slider.max;
+    // rangeBullet.style.left = (bulletPosition * 578) + "px";
+    bullet.style.left = `${bulletPosition * 578}px`;
+    console.log(`slider.value is ${slider.value}\nbullet is ${bullet}\nbulletPosition is ${bulletPosition}}`);
+  }
+
+  render() {
+    return (
+      <div className={sliderStyles.container}>
+        <div className={sliderStyles.rangeSlider}>
+          <span
+            ref={node => this.bullet = node}
+            // id="rs-bullet"
+            className={sliderStyles.rsLabel}>0</span>
+          <input
+            ref={node => this.input = node}
+            onInput={this.showSliderValue}
+            // id="rs-range-line"
+            className={sliderStyles.rsRange}
+            type="range"
+            value="0" min="0" max="200" />
+        </div>
+
+        <div className={sliderStyles.boxMinmax}>
+          <span>0</span><span>200</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Slider;
